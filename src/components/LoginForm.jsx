22a873f650799931b24e43944f3f0be5980dbd5c
Sonar <w24011@osfl.ac.jp>
2025-10-23 10:58:46 +0900

    change commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,7 +4,7 @@ const LoginForm = () => {
     return (
         <form action={doSocialLogin}>
             <button className="bg-pink-400 text-white p-1 rounded-md m-1 text-lg" type="submit" name="action" value="google">
-                Sign In With Google
+                change with google
             </button>
 
             <button className="bg-black text-white p-1 rounded-md m-1 text-lg" type="submit" name="action" value="github">
